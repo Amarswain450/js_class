d27ff3929f3489f30ebb3f95ee0c548343d061de
Amarswain450 <amarswain450@gmail.com>
2023-12-27 22:29:31 +0530

    initial commit

---
+++ 27_12_2023.js
@@ -0,0 +1,48 @@
+// what is javascript
+//  1---> client side scripting language (reactjs, angualr, vuejs)
+// 2----> dynamic content
+// 3---> event handling(onclick, mouseover, onsubmit, mouseleave)
+// 4---> asynchronous programming
+// 5----> cross browser capability
+// 6----> server side development(nodejs)
+// 7-----> bibraries and frameworks(reactjs, angular, vue, expressjs)
+
+// variables
+
+//where we store the value and any type of values
+// var, let, const
+
+//datatypes
+//string
+//integer
+//boolean  // premitive data types
+//undefined
+//null
+
+//array
+//object // non-primitive data types
+
+// var/let/const name = "babulu";
+
+// var name = "babulu";
+// // '' "" ``
+// console.log("my name", name);
+// var age = 23;
+// console.log("my age", age);
+// var isMale = true;
+// console.log("checked", isMale);
+
+// var firstName;
+// firstName = "amar";
+// console.log(firstName); // define a varable name but bot assign any value
+// var lastName = null;
+// lastName = "swain";
+// console.log(lastName);
+
+//string
+//number of words binds inside a quotes i.e. called as string
+// "mmmmmmm"
+// 'mmmmmm'
+// `mmmmmmm`
+// '1111111111111'
+// "true"
