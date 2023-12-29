74fa14e1f293ca6b0cf8de58d6bcdf3dec39b81c
Amarswain450 <amarswain450@gmail.com>
2023-12-29 15:55:17 +0530

    - Loop with sting practice - Amar

---
+++ 29_12_2023.js
@@ -0,0 +1,82 @@
+// string
+// var firstName = "Amar";
+// var lastName = "Swain";
+// console.log(firstName);
+// console.log(lastName);
+// console.log("My name is " + firstName + " " + lastName);
+// console.log(`My name is ${firstName} ${lastName}`);
+
+
+//javascript
+//breden eich - 1996
+// 1997 - ECMA // core javascript // ES5
+// 2015 - ES6 // popular
+
+//difference between var, let & const
+// const firstName = "amar";
+// function myName(){
+//     const lastName = "swain";
+//     console.log(`My name is ${firstName} ${lastName}`);
+// }
+// // myName();
+// console.log(firstName);
+// console.log(lastName);
+
+//var - global scope -- ES5
+// let/const -local scope -- ES6
+// var - braek the rule
+//let - obey the rule
+
+// const firstName = "amar";
+// firstName = "babulu";
+// console.log(firstName);
+
+//hoisting
+// Hoisting is a js mechanisim where we moved variable declaration at the top of the scope
+// console.log(firstName);
+// var firstName = "amar"
+
+
+//loops
+// for loop
+// for- of ///// array /string ---> return value
+// for-each
+//for -in /// object ---> return index
+// while
+// do---while
+//index - 0
+//length - 1
+
+// var str = "welcome to javascript";
+// console.log(str[0]);
+// for loop syntax
+// for(initialization, condition, increment/decrement){}
+
+// for(let i=0; i<str.length; i++){
+//     console.log(str[i]);
+// }
+
+// for(let val of str){
+//     console.log(val);
+// }
+
+// for(let index in str){
+//     console.log(index);
+// }
+
+// 1-10
+// for(let i=1; i<=10; i++){
+//     console.log("i-----", i);
+// }
+
+// let x=1; //2 //3------- //10
+// while(x <= 10){
+//     console.log("x-----", x);
+//     x++;
+// }
+
+// let x=15;
+// do{
+//     console.log("x-----", x);
+//     x++; 
+// }while(x <= 10)
